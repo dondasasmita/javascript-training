54c9673c5e8dac4854480a874e2db13f677061ce
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-04-30 23:41:59 +0800

    first javascript commit

---
+++ celcius.js
@@ -0,0 +1,6 @@
+let fahrenheit = 110
+let celcius = (fahrenheit - 32) * 0.5556
+
+console.log(celcius)
+
+
---
+++ variables.js
@@ -0,0 +1,8 @@
+let score = 10.5;
+let firstName = 'Donda';
+let lastName = 'Sasmita';
+let bonus = 20;
+
+console.log(score + bonus);
+
+console.log('My name is ' + firstName + ' ' + lastName);
