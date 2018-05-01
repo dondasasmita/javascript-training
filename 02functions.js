06822246245a78f849db82c082841457414830d3
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-01 11:45:09 +0800

    basic functions on arrays

--- 02functions.js
+++ 02functions.js
@@ -1,11 +1,33 @@
+//Declare and instantiate an Array
 const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
 
-// numbers[1] = 'Something'
-
-// console.log(numbers);
-
-//start
+/* BEGINNING FUNCTIONS */
+//shift function to delete the first index
 numbers.shift()
 console.log(numbers);
+
+//unshift to add a value to the first position
+numbers.unshift('Something new')
+console.log(numbers);
+
+/* MIDDLE FUNCTIONS */
+
+//splice function below is to delete value from the index of 2, only 1 value ,and replace with the word Hello
+numbers.splice(2,1,'Hello')
+console.log(numbers);
+
+
+/* END FUNCTIONS */
+
+//pop function is to remove the value at the last index
+numbers.pop()
+console.log(numbers);
+
+//push function is to add the value at the end of the array
+numbers.push('Seven')
+console.log(numbers);
+
+
+
 
 
