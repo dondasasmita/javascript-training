c5c8fed5075d9f42241336b53c5bdcdba007254c
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-07-18 08:43:03 +0700

    simple adding function using for loop

---
+++ Foundations/simpleAdding.js
@@ -0,0 +1,25 @@
+/*
+have the function SimpleAdding(num) add up all the numbers from 1 to num. 
+For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10. 
+For the test cases, the parameter num will be any number from 1 to 1000. 
+*/
+
+const simpleAdding = num => {
+  let total = 0;
+  for (let i = 1; i <= num; i++) {
+    total += i;
+  }
+  return total;
+};
+
+console.log(simpleAdding(4));
+
+// function simpleAdding(num) {
+//   let total = 0;
+//   for (let i = 1; i <= num; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// console.log(simpleAdding(4));
