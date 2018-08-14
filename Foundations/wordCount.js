48504cfba433b232342d16fa0d821bbb352182c0
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-08-14 23:10:49 +0800

    using split method to count num of words

---
+++ Foundations/wordCount.js
@@ -0,0 +1,11 @@
+/* Using the JavaScript language, have the function WordCount(str) take the str
+string parameter being passed and return the number of words the string contains
+(ie. "Never eat shredded wheat" would return 4). Words will be separated by single spaces.
+*/
+
+const wordCount = sentence => {
+  const array = sentence.split(" ");
+  return array.length;
+};
+
+console.log(wordCount("Hello World"));
