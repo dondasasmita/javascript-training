05475d1c28960c955929397345aab9d905320c26
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-09-20 08:32:21 +0800

    fixed bugs where it returned wrong value

--- Foundations/letterCount1.js
+++ Foundations/letterCount1.js
@@ -1,5 +1,4 @@
-/*
-Letter Count I                                                                      *
+/* Letter Count I                                                                      *
 *  Have the function LetterCountI(str) take the str parameter being passed and return  *
 *  the first word with the greatest number of repeated letters. For example:           *
 *  "Today, is the greatest day ever!" should return greatest because it has 2 e's      *
@@ -16,33 +15,64 @@ const letterCountI = string => {
   for (let i = 0; i < words.length; i++) {
     for (let j = 0; j < words[i].length - 1; j++) {
       for (let k = j + 1; k <= words[i].length - 1; k++) {
+        // if the same letter is found
         if (words[i].charAt(j) === words[i].charAt(k)) {
-          // check if wordsWithRepeatedLetters is empty
-          if (wordsWithRepeatingLetters.length == 0) {
+          // check if wordsWithRepeatedLetters is not empty
+          if (wordsWithRepeatingLetters.length !== 0) {
+            // compare the word
+            for (let x = 0; x < wordsWithRepeatingLetters.length; x++) {
+              // If the word exists in the array
+              if (words[i] === wordsWithRepeatingLetters[x].word) {
+                wordsWithRepeatingLetters[x].count += 1;
+                wordsWithRepeatingLetters[x].repeatedLetterFound.push(
+                  words[i].charAt(j).toString()
+                );
+              }
+              // If new, push the word to the array
+              else {
+                wordsWithRepeatingLetters.push({
+                  word: words[i],
+                  repeatedLetterFound: [words[i].charAt(j).toString()],
+                  count: 1
+                });
+                break;
+              }
+            }
+          }
+          // if the array does not exists
+          else {
+            // insert the new word to wordsWithRepeatingLetters array
             wordsWithRepeatingLetters.push({
               word: words[i],
               repeatedLetterFound: [words[i].charAt(j).toString()],
               count: 1
             });
-          } else {
-            if (words[i] !== wordsWithRepeatingLetters[i].word) {
-              wordsWithRepeatingLetters.push({
-                word: words[i],
-                repeatedLetterFound: [words[i].charAt(j).toString()],
-                count: 1
-              });
-            } else {
-              wordsWithRepeatingLetters[i].count += 1;
-              wordsWithRepeatingLetters[i].repeatedLetterFound.push(
-                words[i].charAt(j).toString()
-              );
-            }
           }
         }
       }
     }
   }
-  return wordsWithRepeatingLetters;
+  return findTheGreatest(wordsWithRepeatingLetters);
 };
 
-console.log(letterCountI("Today, is the greatest day ever!"));
+// Function to find the greatest value in the array
+const findTheGreatest = array => {
+  let isTheGreatest;
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i].count > array[i + 1].count) {
+      isTheGreatest = array[i].word;
+      // } else {
+      //   return -1;
+    }
+  }
+  if (isTheGreatest != null) {
+    return isTheGreatest;
+  } else {
+    return -1;
+  }
+};
+
+// Running the function
+console.log(letterCountI("Today, is the greatest day ever!")); // returns greatest
+console.log(letterCountI("Donda is not the only one that has double letters")); // returns -1
+console.log(letterCountI("Dondaaxx is the only one that has double letters")); // returns Dondaaxx
