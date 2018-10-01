922d48ec1e3e7a3904ce090f97c6a91377940c18
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-10-01 15:00:44 +0800

    fixed codes when only 2 nums in an array

--- Foundations/secondGreatLow.js
+++ Foundations/secondGreatLow.js
@@ -8,12 +8,44 @@
 */
 
 const secondGreatLow = array => {
-  // order array from lowest to biggest
+  // Order array from lowest to biggest
   let arrayInOrder = array.sort((a, b) => {
     return a - b;
   });
-  return arrayInOrder;
+
+  // Variables to store the nums
+  let lowestNum = arrayInOrder[0];
+  let greatestNum = arrayInOrder[arrayInOrder.length - 1];
+  let secondLowestNum = 0;
+  let secondGreatestNum = 0;
+
+  // Get the secondLowestNum
+  for (let i = 1; i < arrayInOrder.length; i++) {
+    if (arrayInOrder[i] > lowestNum && arrayInOrder[i] < arrayInOrder[i + 1]) {
+      secondLowestNum = arrayInOrder[i];
+      break;
+    }
+  }
+  // Get the secondGreatest
+  for (let i = arrayInOrder.length - 1; i > 0; i--) {
+    if (
+      arrayInOrder[i] < greatestNum &&
+      arrayInOrder[i] > arrayInOrder[i - 1]
+    ) {
+      secondGreatestNum = arrayInOrder[i];
+      break;
+    }
+  }
+
+  // Return statement
+  if (secondLowestNum !== 0 && secondGreatestNum !== 0) {
+    return `${secondLowestNum} ${secondGreatestNum}`;
+  } else {
+    return `${lowestNum} ${greatestNum}`;
+  }
 };
 
 const randomArray = [7, 7, 12, 98, 106];
+const randomArray2 = [7, 106];
 console.log(secondGreatLow(randomArray));
+console.log(secondGreatLow(randomArray2));
