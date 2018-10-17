ad035d84e9e4f9e38f16916c7632dd027189da23
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-10-17 08:37:27 +0700

    function to find prime number

---
+++ Intermediate/primeTime.js
@@ -0,0 +1,24 @@
+/*
+ * Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter
+ * is a prime number, otherwise return the string false. The range will be between 1 and 2^16.
+*/
+
+// a function that finds factors
+const findFactor = num =>
+  Array.from(Array(num + 1), (_, i) => i).filter(i => num % i === 0);
+
+// function that finds prime number
+const PrimeTime = num => {
+  let array = findFactor(num);
+  if (array[0] == 1 && array[1] == num) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(PrimeTime(2)); // True
+console.log(PrimeTime(19)); // True
+console.log(PrimeTime(17)); // True
+console.log(PrimeTime(12)); // False
+console.log(PrimeTime(5)); // True
